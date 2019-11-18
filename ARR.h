7f15d77508b42cc3022439677dde108eca0e6101
Dom26 <50036427+Dom26@users.noreply.github.com>
2019-11-18 02:41:16 -0800

    Added ARR.h and main.cpp files

---
+++ ARR.h
@@ -0,0 +1,13 @@
+/*
+  ARR.h
+  Header file for the array of Linear linked lists
+  Dominique Moore
+*/
+
+class Node{
+
+};
+
+class ARR{
+
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+  main.cpp
+  main file that calls functions to manipulate an array of
+  Linear linked lists.
+  Dominique Moore
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
